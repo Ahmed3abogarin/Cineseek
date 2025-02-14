6485612eff50f8acab1f97af48c8d61343f82373
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-14 15:28:35 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/news_navigator/MoviesBottomNavigation.kt
+++ app/src/main/java/com/movies/cinemix/presentation/news_navigator/MoviesBottomNavigation.kt
@@ -26,19 +26,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Call
-import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -63,7 +55,7 @@ import kotlinx.coroutines.delay
 @Composable
 fun MoviesBottomNav(
     bottomItems: MutableList<BottomItem>,
-    onItemClicked: (Int) -> Unit
+    onItemClicked: (Int) -> Unit,
 ) {
     val configuration = LocalConfiguration.current
 
@@ -75,7 +67,7 @@ fun MoviesBottomNav(
 
     val indicatorOffset by animateIntOffsetAsState(
         targetValue = IntOffset(
-            bottomItems[selectedIndex.intValue].offset.x.toInt() + (bottomItems[selectedIndex.value].size.width / 4) - (bottomItems.count() * 2) + (-2),
+            bottomItems[selectedIndex.intValue].offset.x.toInt() + (bottomItems[selectedIndex.intValue].size.width / 4) - (bottomItems.count() * 2) + (-2),
             15
         ), animationSpec = tween(400)
     )
@@ -136,8 +128,8 @@ fun MoviesBottomNav(
                             },
                             onClick = {
                                 switching.value = true
-                                selectedIndex.value = index
-                                onItemClicked(selectedIndex.value)
+                                selectedIndex.intValue = index
+                                onItemClicked(selectedIndex.intValue)
                             }
                         ),
                     contentAlignment = Alignment.Center
