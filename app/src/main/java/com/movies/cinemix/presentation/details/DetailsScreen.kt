042ad16afb6690875bdb418341c0b05456ece057
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-17 11:36:33 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/movies/cinemix/presentation/details/DetailsScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -29,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -99,7 +97,7 @@ fun DetailsScreen(
 
                 Column(
                     modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp, top = 16.dp,)
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp)
 
                 ) {
                     Row(
