212377b9fad6677983efd7727090c5867fb65346
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-20 15:45:57 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
@@ -1,21 +1,29 @@
 package com.movies.cinemix.presentation.favorite
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.movies.cinemix.domain.model.Movies
+import com.movies.cinemix.presentation.common.EmptyScreen
 import com.movies.cinemix.presentation.common.MovieList
+import com.movies.cinemix.presentation.seeall.MovieCard2
+import com.movies.cinemix.ui.theme.MyColor
 
 @Composable
 fun FavoriteScreen(
     state: FavoriteState,
-    onClick: (Movies) -> Unit
+    onClick: (Movies) -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -24,14 +32,34 @@ fun FavoriteScreen(
             .padding(top = 24.dp, start = 24.dp, end = 24.dp)
     ) {
         Text(text = " Favorite screen")
+        val movies = state.favoriteMovies
 
-        if(state.favoriteMovies.isNotEmpty()){
-            Log.v("MOVIES",state.favoriteMovies[0].toString())
-            Log.v("MOVIES","size: "+state.favoriteMovies.size.toString())
+        if (movies.isEmpty()) {
+            EmptyScreen()
         }
 
+        Box(
+            modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxSize()
+        ) {
 
-        MovieList(moviesList = state.favoriteMovies, onClick = {onClick(it)})
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        MyColor
+                    ),
+                columns = StaggeredGridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(state.favoriteMovies.size) { page ->
+                    MovieCard2(state.favoriteMovies[page], onClick = { onClick(movies[page]) })
+                }
+            }
+        }
+
+        MovieList(moviesList = state.favoriteMovies, onClick = { onClick(it) })
     }
 }
 
