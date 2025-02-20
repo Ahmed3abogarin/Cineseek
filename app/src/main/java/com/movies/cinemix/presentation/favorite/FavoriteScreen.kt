9d35a94d7726fc5584593e0c45327355edf47c3b
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-20 17:23:26 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.movies.cinemix.domain.model.Movies
 import com.movies.cinemix.presentation.common.EmptyScreen
@@ -33,12 +37,13 @@ fun FavoriteScreen(
             .statusBarsPadding()
             .padding(top = 24.dp)
     ) {
-        Text(text = " Favorite screen")
+        Text(text = " Favorite", style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold, color = Color.White), modifier = Modifier.padding(start = 24.dp))
         val movies = state.favoriteMovies
 
         if (movies.isEmpty()) {
             EmptyScreen()
         }
+        Spacer(modifier = Modifier.height(20.dp))
 
         Box(
             modifier = Modifier
