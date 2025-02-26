2e268c66aae58005248461df6f561f6c01fefee9
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-27 00:07:30 +0300

    الحمد لله

---
+++ app/src/main/java/com/movies/cinemix/presentation/common/FavoriteList.kt
@@ -0,0 +1,158 @@
+package com.movies.cinemix.presentation.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.movies.cinemix.R
+import com.movies.cinemix.domain.model.Movies
+import com.movies.cinemix.ui.theme.BottomColor
+import com.movies.cinemix.ui.theme.Gold
+import com.movies.cinemix.ui.theme.MyRed
+
+
+@Composable
+fun FavoriteList(
+    movies: List<Movies>,
+    onClick: (Movies) -> Unit,
+) {
+
+    LazyColumn (
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+    ){
+        items(movies.size) {
+            movies[it].let { movie ->
+                val currentMovie = movies[it]
+                Column {
+                    FavoriteCard(movie = movie, onClick = { onClick(currentMovie) })
+
+                }
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun FavoriteCard(
+    movie: Movies,
+    onClick: () -> Unit,
+) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = Modifier
+            .height(190.dp)
+            .fillMaxWidth()
+
+            .clickable {
+                onClick()
+            }
+            .clip(RoundedCornerShape(8.dp)).background(BottomColor)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+
+            AsyncImage(
+                model = ImageRequest.Builder(context)
+                    .data("https://image.tmdb.org/t/p/w500/" + movie.poster_path)
+                    .build(),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(155.dp)
+                    .width(103.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.FillBounds
+            )
+            Spacer(modifier = Modifier.width(25.dp))
+            Column {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.bodyLarge.copy(color = Color.White)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = movie.overview,
+                    style = MaterialTheme.typography.bodySmall.copy(color = Color.White),
+                    maxLines = 3
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.ic_time), contentDescription = null
+                    )
+                    Text(text = "2h33m",fontSize = 12.sp, color = Color.White)
+                    Icon(
+                        Icons.Default.Star, contentDescription = null, tint = Gold,
+                        modifier = Modifier.size(18.dp)
+                    )
+                    Text(text = "7.6", color = Gold , fontSize = 12.sp)
+                }
+                Spacer(modifier = Modifier.height(3.dp))
+                Button(
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.padding(start = 4.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MyRed),
+                    onClick = {  }
+                ) {
+                    Text(
+                        text = "Watch Trailer",
+                        modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+                    )
+                    Image(painter = painterResource(R.drawable.ic_play), contentDescription = null, modifier = Modifier.size(35.dp))
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+
+}
+
+//@Preview
+//@Composable
+//fun FavoriteCardPreView() {
+//    CinemixTheme {
+//        FavoriteCard()
+//    }
+//
+//}
--- app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/movies/cinemix/presentation/favorite/FavoriteScreen.kt
@@ -1,18 +1,12 @@
 package com.movies.cinemix.presentation.favorite
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,8 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.movies.cinemix.domain.model.Movies
 import com.movies.cinemix.presentation.common.EmptyScreen
-import com.movies.cinemix.presentation.common.MovieList
-import com.movies.cinemix.presentation.seeall.MovieCard2
+import com.movies.cinemix.presentation.common.FavoriteList
 import com.movies.cinemix.ui.theme.MyColor
 
 @Composable
@@ -32,15 +25,22 @@ fun FavoriteScreen(
     onClick: (Movies) -> Unit,
 ) {
     Column(
-        modifier = Modifier.background(
-            MyColor
-        )
-            .statusBarsPadding()
-            .verticalScroll(rememberScrollState())
-            .padding(top = 24.dp)
+        modifier = Modifier
             .fillMaxSize()
+            .background(
+                MyColor
+            )
+            .statusBarsPadding()
+            .padding(top = 24.dp)
     ) {
-        Text(text = " Favorite", style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold, color = Color.White), modifier = Modifier.padding(start = 24.dp))
+        Text(
+            text = " Favorite",
+            style = MaterialTheme.typography.displayMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            modifier = Modifier.padding(start = 24.dp)
+        )
         val movies = state.favoriteMovies
 
         if (movies.isEmpty()) {
@@ -48,29 +48,12 @@ fun FavoriteScreen(
         }
         Spacer(modifier = Modifier.height(10.dp))
 
-        Box(
-            modifier = Modifier
-                .background(Color.Magenta)
-                .fillMaxSize()
-        ) {
 
-            LazyVerticalStaggeredGrid(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        MyColor
-                    ),
-                columns = StaggeredGridCells.Fixed(2),
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                items(state.favoriteMovies.size) { page ->
-                    MovieCard2(state.favoriteMovies[page], onClick = { onClick(movies[page]) })
-                }
-            }
-        }
-        MovieList(moviesList = state.favoriteMovies, onClick = { onClick(it) })
+        FavoriteList(movies = movies, onClick = { onClick(it) })
 
-        Spacer(modifier = Modifier.height(110.dp))
+
+
+        Spacer(modifier = Modifier.height(110.dp).weight(1f))
 
     }
 }
