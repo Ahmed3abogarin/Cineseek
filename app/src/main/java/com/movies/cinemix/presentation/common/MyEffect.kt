b7d00fec7b7031822dae6ba9d16f0846eef46ac1
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-02-28 21:49:10 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/common/MyEffect.kt
+++ app/src/main/java/com/movies/cinemix/presentation/common/MyEffect.kt
@@ -1,16 +1,9 @@
 package com.movies.cinemix.presentation.common
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
