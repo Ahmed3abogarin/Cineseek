3bb21597be024668b7f34bb9244b14d33bc16074
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-02 23:48:36 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/MainActivity.kt
+++ app/src/main/java/com/movies/cinemix/MainActivity.kt
@@ -4,18 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavGraph
 import com.movies.cinemix.presentation.navGraph.NavGraph
-import com.movies.cinemix.presentation.news_navigator.MoviesBottomNav
 import com.movies.cinemix.ui.theme.CinemixTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,14 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CinemixTheme {
-//                Surface {
-////                    ViewPagerSlider2()
-//                    //YouTubeDialog("WTs49l7IGg0", LocalLifecycleOwner.current)
-//                    HomeScreen("WTs49l7IGg0", LocalLifecycleOwner.current)
-//                }
-
-                    NavGraph()
-
+                NavGraph()
             }
         }
     }
