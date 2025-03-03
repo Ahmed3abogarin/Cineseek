01048aeaad65b010c17b9997c93bc4bd7b30e430
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-03 17:38:49 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/MainActivity.kt
+++ app/src/main/java/com/movies/cinemix/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.movies.cinemix
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
 import com.movies.cinemix.presentation.navGraph.NavGraph
 import com.movies.cinemix.ui.theme.CinemixTheme
+import com.movies.cinemix.ui.theme.MyColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +20,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CinemixTheme {
-                NavGraph()
+                Box(modifier = Modifier.background(MyColor)) {
+                    NavGraph()
+                }
+
+
             }
         }
     }
