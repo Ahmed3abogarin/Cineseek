2841e60b6d745d3bba9d90f1c0be5fcbef42b38e
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-06 17:38:59 +0300

    الحمد لله

--- app/src/main/java/com/movies/cinemix/presentation/seeall/SeeAllViewModel.kt
+++ app/src/main/java/com/movies/cinemix/presentation/seeall/SeeAllViewModel.kt
@@ -1,25 +1,39 @@
 package com.movies.cinemix.presentation.seeall
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.paging.PagingData
-import com.movies.cinemix.domain.model.Movies
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.movies.cinemix.domain.usecases.MoviesUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
 @HiltViewModel
 class SeeAllViewModel @Inject constructor(
     private val moviesUseCases: MoviesUseCases,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    fun getMovies(category: String): Flow<PagingData<Movies>> {
-        return when (category) {
+
+    private val _state = mutableStateOf(SeeAllState())
+    val state: State<SeeAllState> = _state
+
+    init {
+        savedStateHandle.get<String>("movie_category")?.let { category ->
+            getMovies(category)
+        }
+    }
+
+    fun getMovies(category: String){
+        val list = when (category) {
             "nowPlaying" -> moviesUseCases.getNowPlayingMovies.invoke()
             "topRated" -> moviesUseCases.getTopRatedMovies.invoke()
             "upcoming" -> moviesUseCases.getUpcomingMovies.invoke()
             "popular" -> moviesUseCases.getPopularMovies.invoke()
             "arabic" -> moviesUseCases.getArabicMovies.invoke()
+            "marvel" -> moviesUseCases.getMarvelMovies.invoke()
             "Action" -> moviesUseCases.getGenreMovies.invoke(genreNum = 28)
             "Adventure" -> moviesUseCases.getGenreMovies.invoke(genreNum = 12)
             "Family" -> moviesUseCases.getGenreMovies.invoke(genreNum = 10751)
@@ -32,6 +46,7 @@ class SeeAllViewModel @Inject constructor(
             "War" -> moviesUseCases.getGenreMovies.invoke(genreNum = 10752)
             "Fantasy" -> moviesUseCases.getGenreMovies.invoke(genreNum = 14)
             else -> moviesUseCases.getPopularMovies.invoke()
-        }
+        }.cachedIn(viewModelScope)
+        _state.value = _state.value.copy(movies = list)
     }
 }
